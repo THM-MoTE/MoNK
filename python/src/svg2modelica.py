919fd6de09ff2d2b421caa67a6561b00609e3c35
Christopher Schoelzel <christopher.schoelzel@mni.thm.de>
2018-11-23 15:35:21 +0100

    finishes transition from nice argparse to ugly getopt (damn you, python 2.6!)

--- python/src/svg2modelica.py
+++ python/src/svg2modelica.py
@@ -5,17 +5,16 @@ import sys
 
 if __name__ == '__main__':
   try:
-    optlist, args = getopt.getopt(args, "m:s", ["modelname=", "strict"])
+    opts, args = getopt.getopt(sys.argv[1:], "m:s", ["modelname=", "strict"])
   except getopt.GetoptError as err:
     print(str(err))
     print("usage: python svg2modelica.py [-m modelname] [-s]")
     sys.exit(1)
-  desc = "Converts SVG file to Modelica annotation."
-  parser = argparse.ArgumentParser(description=desc)
-  mhelp = "name of the model (should be the same as output file name)"
-  parser.add_argument("-m", "--modelname", help=mhelp)
-  shelp = "non-translatable elements will throw an exception " + \
-          "instead of being ignored"
-  parser.add_argument("--strict", "-s", action="store_true", help=shelp)
-  args = parser.parse_args()
-  print("foo %s %s" % (args.strict, args.modelname))
+  strict = False
+  modelname = "DummyModel"
+  for k, v in opts:
+    if k in ("-s", "--strict"):
+      strict = True
+    elif k in ("-m", "--modelname"):
+      modelname = v
+  print("foo %s %s" % (strict, modelname))
