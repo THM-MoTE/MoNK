84ae8316d393f14c2815455e7c8fcdbfbeef760c
Christopher Schoelzel <christopher.schoelzel@mni.thm.de>
2018-11-23 15:45:01 +0100

    for some strange reason, inkscape also delegates boolean parameters with an argument

--- python/src/svg2modelica.py
+++ python/src/svg2modelica.py
@@ -5,16 +5,16 @@ import sys
 
 if __name__ == '__main__':
   try:
-    opts, args = getopt.getopt(sys.argv[1:], "m:s", ["modelname=", "strict"])
+    opts, args = getopt.getopt(sys.argv[1:], "m:s:", ["modelname=", "strict="])
   except getopt.GetoptError as err:
     print(str(err))
-    print("usage: python svg2modelica.py [-m modelname] [-s]")
     sys.exit(1)
+    print("usage: python svg2modelica.py [-m modelname] [-s true/false]")
   strict = False
   modelname = "DummyModel"
   for k, v in opts:
     if k in ("-s", "--strict"):
-      strict = True
+      strict = bool(v)
     elif k in ("-m", "--modelname"):
       modelname = v
   print("foo %s %s" % (strict, modelname))
