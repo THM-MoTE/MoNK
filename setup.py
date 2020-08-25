1167beec42c02783760fe9a9ac9bf030dc49250b
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2020-08-25 13:46:47 +0200

    does not use exist_ok, since this is not available in python 2.7

--- setup.py
+++ setup.py
@@ -115,7 +115,8 @@ class InstallToExtensionDir(install):
 
     def run(self):
         uext = determine_user_ext(forcedefault=self.defaultext)
-        os.makedirs(uext, exist_ok=True)
+        if not os.path.exists(uext):
+            os.makedirs(uext)
         # copy source files to extension dir
         for f in [x for x in glob.glob("src/**") if ".egg-info" not in x]:
             src = f
