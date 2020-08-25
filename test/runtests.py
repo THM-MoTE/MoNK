df1caf029f1f074c800d8c236d54ff49d5fb2e27
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2020-08-25 15:32:13 +0200

    convers path to string for python 2.7

--- test/runtests.py
+++ test/runtests.py
@@ -21,5 +21,5 @@ class TestSvg2Modelica(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    os.chdir(pathlib.Path(__file__).parents[1])
+    os.chdir(str(pathlib.Path(__file__).parents[1]))
     unittest.main()
