bab00fca8c3d51b3d4070b019c6ef98cc2f894ae
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2020-12-04 14:20:41 +0100

    only imports future stuff if we actually are on python 2.7

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import (absolute_import, division,
-                        print_function)
-from builtins import *
-
 import sys
 import os
 import glob
@@ -11,6 +7,12 @@ import tarfile
 import zipfile
 from setuptools import setup, Command
 import io
+
+if sys.version_info[0] < 3:
+    from __future__ import (absolute_import, division,
+                            print_function)
+    from builtins import *
+
 
 data_files = [
     (os.path.relpath(os.path.dirname(x), "res"), [x])
